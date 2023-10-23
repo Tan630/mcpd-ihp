071a060ce47cb30ded8bd392ccb0f44657886d49
user <user@example.com>
2023-10-23 01:07:56 -0400

    update definition of play

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -33,5 +33,11 @@ CREATE TRIGGER update_players_updated_at BEFORE UPDATE ON players FOR EACH ROW E
 CREATE INDEX apps_created_at_index ON apps (created_at);
 CREATE TRIGGER update_apps_updated_at BEFORE UPDATE ON apps FOR EACH ROW EXECUTE FUNCTION set_updated_at_to_now();
 CREATE TABLE play (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    consent BOOLEAN NOT NULL,
+    play_data JSONB NOT NULL,
+    player_name TEXT NOT NULL,
+    app_name TEXT NOT NULL
 );
